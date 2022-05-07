6effbb400afba39d2304811d4c42e2281b64e1e5
Дмитрий Александрович Худокормов <s0163526@edu.kubsu.ru>
2022-05-07 14:32:27 +0300

    lb2, task 11

--- lab12.pl
+++ lab12.pl
@@ -14,3 +14,21 @@ kolvo_vp_delD(N,Count):-rec2(N,Count,N,0).
 rec2(N,R,0,R):-!.
 rec2(N,Count,I,R):- I1 is I-1,(0 is I mod 2,not(nod(N,I,1)),R1 is R+1,rec2(N,Count,I1,R1),!;rec2(N,Count,I1,R)),!.
 %2
+sumLess5(0,0):-!.
+sumLess5(A,Sum):-(A mod 10)<5,!,
+    A1 is A div 10,S is (A mod 10),sumLess5(A1,Sum1),Sum is Sum1+S.
+sumLess5(A,Sum):-A1 is A div 10, sumLess5(A1,Sum).
+
+minDel(_,_,0):-!.
+minDel(A,Del,I):- 0 is A mod I,!,
+    Del is I, minDel(A,Del,0).
+minDel(A,Del,I):- I1 is I +1, minDel(A,Del,I1).
+minDel(1,1):-!.
+minDel(0,1):-!.
+minDel(A,Del):- minDel(A,Del,2).
+
+findNum(_,_,_,1):-!,fail.
+findNum(A,X,Del,I):- not(nod(A,I,1)),not(0 is (I mod Del)),!,
+    X is I.
+findNum(A,X,Del,I):- I1 is I - 1, findNum(A,X,Del,I1).
+findNum(A,X):- minDel(A,Del),I is A - 1,findNum(A,X1,Del,I),sumLess5(A,Y),X is X1 * Y.
