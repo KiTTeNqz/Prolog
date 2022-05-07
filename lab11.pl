5bd07e391022725ba3c74de4a79fb4ee0a993074
Дмитрий Александрович Худокормов <s0163526@edu.kubsu.ru>
2022-05-07 11:45:40 +0300

    19

--- lab11.pl
+++ lab11.pl
@@ -85,3 +85,13 @@ maxD(X,N):-maxD(X,0,N).
 maxD(0,Y,Y):-!.
 maxD(X,P,N):-D is X mod 10,D mod 3 =\= 0, (D>P, P1 is D; P1 is P),X1 is X div 10,maxD(X1,P1,N),!;
     X2 is X div 10,maxD(X2,P,N). 
+%19
+fib(0,0):-!.
+fib(1,1):-!.
+fib(2,1):-!.
+fib(N,X):-
+    N1 is N-1,
+    N2 is N-2,
+    fib(N1,X1),
+    fib(N2,X2),
+    X is X1+X2.
